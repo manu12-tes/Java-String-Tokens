94c509c4e3d81b896e0ff6c17950f2300d30bd28
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-13 00:30:55 -0500

    Agregar archivos de proyecto.

---
+++ Java-String-Tokens/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Java_String_Tokens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame una cadena");
+            String cadena = Console.ReadLine();
+            String[] cadenas = Regex.Split(cadena,"/t");
+
+            Console.WriteLine(cadenas.Length);
+            foreach (string cad in cadenas)
+            {
+                Console.WriteLine(cad);
+            }
+        }
+    }
+}
